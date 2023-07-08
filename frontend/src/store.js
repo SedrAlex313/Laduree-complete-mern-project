8967bc81839dded5fd076318f213c4ec4080c4f6
SedrAlex313 <124774403+SedrAlex313@users.noreply.github.com>
2023-07-08 01:11:48 -0400

    finish laduree menu

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,11 +3,12 @@ import thunk from "redux-thunk";
 import { applyMiddleware,combineReducers } from "redux";
 import { placeOrderReducer } from "./reducers/orderReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
-
+import { getAllMealsReducer } from './reducers/mealReducer';
 
 //root-reducer
 const rootReducer = combineReducers({
     placeOrderReducer: placeOrderReducer,
+    getAllMealsReducer: getAllMealsReducer,
 
 })
  const middleware= [thunk];
